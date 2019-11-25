5db8d3cab5a86549247a0fd570082b09cbd856cd
lluc <cashroll720@gmail.com>
2019-11-25 17:42:25 +0100

    commit public

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,6 @@ const servidor = express()
 
 servidor.use(express.static("public"))
 
-servidor.listen(3000,()=>{
-    console.log("servidor en puerto 3000")
+servidor.listen(80,()=>{
+    console.log("servidor en puerto 80")
 })
