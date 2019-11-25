0d8e182fb6035b94fe83db3d0edd5314c84c90d8
lluc <cashroll720@gmail.com>
2019-11-25 17:01:00 +0100

    destroy for deploy

---
+++ newspaper/backend/index.js
@@ -0,0 +1,11 @@
+
+
+const express = require("express")
+
+const servidor = express()
+
+servidor.use(express.static("public"))
+
+servidor.listen(3000,()=>{
+    console.log("servidor en puerto 3000")
+})
